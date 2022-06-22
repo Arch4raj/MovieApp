71e96cdb62b2e968b761e590f82224c3f6ca6729
Arch4raj <archanasoman93@gmail.com>
2022-06-22 10:25:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -124,42 +124,10 @@
 
 // Add color----------->
 
-// import { color } from '@mui/system';
+import { color } from '@mui/system';
 // import { useState } from 'react';
 // import './App.css';
-// function Addcolor(){
-//     const [color,setColor]=useState();
-//     const initialcolor=["green","blue"]
-//     const[colorlist,setColorlist]=useState(initialcolor);
-//     const styles={
-//         backgroundColor:color,
-//     };
-//     return(
-// <div className='color-input'>
-// <input style={styles} onChange={(event)=>setColor(event.target.value)} placeholder='name'></input>
 
-// <button onClick={()=>setColorlist([...colorlist,color])}>Add Color</button>
-//    {colorlist.map((clr)=> <Colorbox color={clr} />)} 
-
-// </div>
-//     );
-
-   
-// }export default Addcolor;
-
-// function Colorbox({color}){
-//     const styles={
-//         backgroundColor:color,
-//         height:"25px",
-//         width:"250px"
-        
-//     };
-//     return(
-//         <div style={styles}>
-
-//         </div>
-//     );
-// }
 
 
 //Add movie---------->
@@ -303,7 +271,7 @@
 
 import './App.css';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Routes,Route,Link,useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import IconButton from '@mui/material/IconButton';
@@ -318,10 +286,8 @@ import TextField from '@mui/material/TextField';
 import { toBeEmpty } from '@testing-library/jest-dom/dist/matchers';
 function App(){
     
-const initialmovielist=[
-
-    {
-    
+const initialmovielist=[{
+    "id":'100',
     "name": "RRR",
     "poster":
     "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG",
@@ -330,6 +296,7 @@ const initialmovielist=[
     "trailer": "https://www.youtube.com/embed/f_vbAtFSEc0"
     },
     {
+        "id":'101',
     "name": "Iron man 2",
     "poster": "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
     "rating": 7,
@@ -337,7 +304,7 @@ const initialmovielist=[
     "trailer": "https://www.youtube.com/embed/wKtcmiifycU"
     },
     {
-    
+        "id":'102',
     "name": "No Country for Old Men",
     "poster": "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
     "rating": 8.1,
@@ -345,7 +312,7 @@ const initialmovielist=[
     "trailer": "https://www.youtube.com/embed/38A__WT3-o0"
     },
     {
-    
+        "id":'103',
     "name": "Jai Bhim",
     "poster": "https://m.media-amazon.com/images/M/MV5BY2Y5ZWMwZDgtZDQxYy00Mjk0LThhY2YtMmU1MTRmMjVhMjRiXkEyXkFqcGdeQXVyMTI1NDEyNTM5._V1_FMjpg_UX1000_.jpg",
     "summary": "A tribal woman and a righteous lawyer battle in court to unravel the mystery around the disappearance of her husband, who was picked up the police on a false case",
@@ -353,7 +320,7 @@ const initialmovielist=[
     "trailer": "https://www.youtube.com/embed/nnXpbTFrqXA"
     },
     {
-    
+        "id":'104',
     "name": "The Avengers",
     "rating": 8,
     "summary": "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
@@ -361,7 +328,7 @@ const initialmovielist=[
     "trailer": "https://www.youtube.com/embed/eOrNdBpGMv8"
     },
     {
-    
+        "id":'105',
     "name": "Interstellar",
     "poster": "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
     "rating": 8.6,
@@ -369,7 +336,7 @@ const initialmovielist=[
     "trailer": "https://www.youtube.com/embed/zSWdZVtXT7E"
     },
     {
-    
+        "id":'106',
     "name": "Baahubali",
     "poster": "https://flxt.tmsimg.com/assets/p11546593_p_v10_af.jpg",
     "rating": 8,
@@ -377,7 +344,7 @@ const initialmovielist=[
     "trailer": "https://www.youtube.com/embed/sOEg_YZQsTI"
     },
     {
-    
+        "id":'107',
     "name": "Ratatouille",
     "poster": "https://resizing.flixster.com/gL_JpWcD7sNHNYSwI1ff069Yyug=/ems.ZW1zLXByZC1hc3NldHMvbW92aWVzLzc4ZmJhZjZiLTEzNWMtNDIwOC1hYzU1LTgwZjE3ZjQzNTdiNy5qcGc=",
     "rating": 8,
@@ -394,75 +361,110 @@ const navigate= useNavigate()
          
           <Button color="inherit" onClick={()=>navigate('/movies')}>movies</Button>
           <Button color="inherit" onClick={()=>navigate('/movies/add')}>AddMovies</Button>
-          {/* <Button color="inherit" onClick={()=>navigate('/tictac')}>tic_tac_toe</Button> */}
+          <Button color="inherit" onClick={()=>navigate('/tictac')}>tic_tac_toe</Button>
+          <Button color="inherit" onClick={()=>navigate('/color')}>Add color</Button>
         </Toolbar>
       </AppBar>
             {/* <Link to="/movies">movie</Link> */}
        <Routes>
-      <Route path="/movies" element={<Movielist movielist={movielist} setmovielist={setmovielist}/>}/>
-      <Route path="/movies/:id" element={<Moviedetails movielist={movielist} />}/>
-      {/* <Route path="/tictac" element={<Tictactoe/>}/> */}
-      <Route path="/movies/add" element={<Addmovie  movielist={movielist} setmovielist={setmovielist} />}/>
+      {/* <Route path="/movies" element={<Movielist movielist={movielist} setmovielist={setmovielist}/>}/> */}
+      <Route path="/movies" element={<Movielist />}/>
+      <Route path="/movies/:id" element={<Moviedetails  />}/>
+      <Route path="/tictac" element={<Tictactoe/>}/>
+      <Route path="/movies/add" element={<Addmovie   />}/>
+      <Route path="/movie/edit/:id" element={<Editmovie   />}/>
+      <Route path="/color" element={<Addcolor   />}/>
        </Routes>
         </div>
     );
 }
 export default App;
 
-function Movielist({movielist,setmovielist}){
+function Movielist(){
+    const [movielist,setmovielist] =useState([]);
+   const getMovies=()=>{
+    fetch("https://62adbe4b402135c7acc6057a.mockapi.io/movies",{method: "GET",})
+    .then((data)=>data.json())
+    .then((mvs)=>setmovielist(mvs));
+   };
    
-        // const [name,Setname]=useState();
-        // const [poster,Setposter]=useState();
-        // const [rate,Setrate]=useState();
-        // const [summary,Setsummary]=useState();
-        // const addmovie=()=>{
-        //     const newmovie = {
-        //         name:name,
-        //        poster:poster,
-        //        rate:rate,
-        //        summary:summary,
-        //     };
-        //     setmovielist([...movielist,newmovie])
+    useEffect(()=> getMovies(),[]); // getmovies called only once when the component is mounted
 
-        // }
-        
-//             <div className='input-movie'>
-//   <TextField  label="name" variant="standard" onChange={(event)=>Setname(event.target.value)} />
-//   <TextField  label="poster" variant="standard" onChange={(event)=>Setposter(event.target.value)} />
-//   <TextField  label="rating" variant="standard" onChange={(event)=>Setrate(event.target.value)} />
-//   <TextField  label="summary" variant="standard" onChange={(event)=>Setsummary(event.target.value)} />
-          
-{/* <input onChange={(event)=>Setname(event.target.value)} placeholder='name'/>
-            <input onChange={(event)=>Setposter(event.target.value)} placeholder='poster'/>
-            <input onChange={(event)=>Setrate(event.target.value)} placeholder='rating'/>
-            <input onChange={(event)=>Setsummary(event.target.value)} placeholder='summary'/>
-            <button onClick={()=>{ 
-                
-                const newmovie = {
-                    name:name,
-                   poster:poster,
-                   rate:rate,
-                   summary:summary,
-                };
-                setmovielist([...movielist,newmovie])
-            }}>Add Movie</button> */}
-
-            // <Button onClick={addmovie} variant="outlined">Add Movie</Button>
-            return(
+    const deleteMovie=(id)=>{
+        fetch(`https://62adbe4b402135c7acc6057a.mockapi.io/movies/${id}`,{method: "DELETE",})
+        .then(()=>getMovies());  //to refresh the data once the content is deleted
+    };
+    const navigate= useNavigate()
+          return(
             <div>
             <div className='movie'>
-            {movielist.map((mv,index)=>(<Movie key={index} movie={mv} id={index} />))}
+            {movielist.map((mv)=>(
+            <Movie key={mv.id} movie={mv} id={mv.id} 
+            deletebutton={<button onClick={()=>deleteMovie(mv.id)}>delete</button>}
+            editbutton={<button onClick={()=>navigate(`/movie/edit/${mv.id}`)}>Edit</button>}  />))}
             </div>
            
            </div >
             
         );
     } 
-    function Addmovie({movielist,setmovielist}){
+    function Editmovie(){
+        const {id}=useParams();
+            
+            const [movie,setmovie] =useState(null);
+   const getMovie=()=>{
+    fetch(`https://62adbe4b402135c7acc6057a.mockapi.io/movies/${id}`,{method: "GET",})
+    .then((data)=>data.json())
+    .then((mv)=>setmovie(mv));
+   };
+   
+    useEffect(()=> getMovie(),[]);
+        
+          return(
+          <div>
+       {movie  ? <Editform movie={movie} /> : "loading.."}
+         </div>
+        )
+    }
+
+function Editform({movie}){
+   
+    const [name,Setname]=useState(movie.name);
+    const [poster,Setposter]=useState(movie.poster);
+    const [rate,Setrate]=useState(movie.rating);
+    const [summary,Setsummary]=useState(movie.summary);
+    const navigate= useNavigate();
+    const addmovie=()=>{
+        const newmovie = {
+            name:name,
+           poster:poster,
+           rate:rate,
+           summary:summary,
+        };
+        fetch(`https://62adbe4b402135c7acc6057a.mockapi.io/movies/${movie.id}`,{
+            method: "PUT",
+            body:JSON.stringify(newmovie),
+            headers:{
+                "Content-Type" :"application/json",
+            }
+        }).then(()=>navigate("/movies"))
+    }
+    return(
+         <div className='input-movie'>
+    <TextField value={name} label="name" variant="standard" onChange={(event)=>Setname(event.target.value)} />
+    <TextField value={poster} label="poster" variant="standard" onChange={(event)=>Setposter(event.target.value)} />
+    <TextField value={rate} label="rate" variant="standard" onChange={(event)=>Setrate(event.target.value)} />
+    <TextField value={summary} label="summary" variant="standard" onChange={(event)=>Setsummary(event.target.value)} />
+    <Button onClick={addmovie} variant="outlined">save</Button>
+    </div>)
+    
+}
+    function Addmovie(){
         const [name,Setname]=useState();
         const [poster,Setposter]=useState();
         const [rate,Setrate]=useState();
         const [summary,Setsummary]=useState();
+        const navigate= useNavigate()
         const addmovie=()=>{
             const newmovie = {
                 name:name,
@@ -470,8 +472,16 @@ function Movielist({movielist,setmovielist}){
                rate:rate,
                summary:summary,
             };
-            setmovielist([...movielist,newmovie])
-
+           
+            fetch(`https://62adbe4b402135c7acc6057a.mockapi.io/movies`,{
+                method: "POST",
+                body:JSON.stringify(newmovie),
+                headers:{
+                    "Content-Type" :"application/json",       //mentioning json data
+                }
+            }).then(()=>navigate("/movies"))
+            // setmovielist([...movielist,newmovie])
+            
         }
         return(
             <div className='input-movie'>
@@ -484,7 +494,7 @@ function Movielist({movielist,setmovielist}){
         );
     }
     
-    function Movie({movie,id}){
+    function Movie({movie,id,deletebutton,editbutton}){
            const [show,setShow]= useState(true)
            const parastyle={display : show ? "block":"none"}
            const navigate= useNavigate()
@@ -507,7 +517,7 @@ function Movielist({movielist,setmovielist}){
                 {/* <button onClick={()=>navigate(`/movies/${id}`)}>info</button> */}
                
                 <p style={parastyle}className='movie-summary'>{movie.summary}</p>
-            <Counter />
+            <Counter />{deletebutton}{editbutton}
 
             </div>
             );
@@ -535,12 +545,20 @@ function Movielist({movielist,setmovielist}){
           
             );
           }
-        function Moviedetails({movielist}){
+        function Moviedetails(){
             const {id}=useParams()
-            const movie=movielist[id];
+            // const movie=movielist[id];
+            const [movie,setmovie] =useState([{}]);
+   const getMovie=()=>{
+    fetch(`https://62adbe4b402135c7acc6057a.mockapi.io/movies/${id}`,{method: "GET",})
+    .then((data)=>data.json())
+    .then((mv)=>setmovie(mv));
+   };
+   
+    useEffect(()=> getMovie(),[]);
         return(
             <div>
-             <iframe width="713"
+             <iframe width="100%"
              height="401" 
              src={movie.trailer} 
              title="YouTube video player" frameborder="0" 
@@ -559,37 +577,81 @@ function Movielist({movielist,setmovielist}){
         // tic-tac-toe---------->
 
 
-        // function Tictactoe(){
+        function Tictactoe(){
 
-        //     return(
-        //         <div >
-        //     <h3>fun game</h3>
-        //     <Board/>
+            return(
+                <div >
+            <h3>fun game</h3>
+            <Board/>
             
             
-        //         </div>
-        //     )
-        // }
-        // function Board(){
-        //     const [board,setBoard]=useState([null,null,null,null,
-        //         null,null,null,null,null,null,null,null])
-        //    const [isxturn,setIsxturn] =useState(true)
-        //         return(
+                </div>
+            )
+        }
+        function Board(){
+            const [board,setBoard]=useState([null,null,null,null,
+                null,null,null,null,null,null,null,null])
+           const [isxturn,setIsxturn] =useState(true)
+
+           const handleClick=(index)=>{
+
+           if(board[index]==null){
+            const boardcopy=[...board]
+            boardcopy[index]= isxturn ? 'x': 'o'
+            setBoard(boardcopy)
+            setIsxturn(!isxturn)
+           }
+             }
+                return(
                 
-        //         <div className='board'>
-        //             {board.map((val,index)=><Gamebox val={index} onPlayerClick={()=>handleClick(index)} />)}
+                <div className='board'>
+                    {board.map((val,index)=><Gamebox val={val} onPlayerClick={()=>handleClick(index)} />)}
             
-        //         </div>
-        //     )
-        // }
-        // function Gamebox({val,onPlayerClick}){
-        //     // const [val,setVal]=useState()
-        //     const style={
-        //         color : val == 'x' ?'green':'red'
-        //     }
-        //     return(
-        //         <div style={style} className='box' onClick={onPlayerClick}>
-        //          {val}
-        //         </div>
-        //     )
-        // }
+                </div>
+            )
+        }
+        function Gamebox({val,onPlayerClick}){
+            // const [val,setVal]=useState()
+            const style={
+                color : val == 'x' ?'green':'red'
+            }
+            return(
+                <div style={style} className='box' onClick={onPlayerClick}>
+                 {val}
+                </div>
+            )
+        }
+        function Addcolor(){
+            const [color,setColor]=useState();
+            const initialcolor=["green","blue"]
+            const[colorlist,setColorlist]=useState(initialcolor);
+            const styles={
+                backgroundColor:color,
+            };
+            return(
+        <div className='color-input'>
+        <input style={styles} onChange={(event)=>setColor(event.target.value)} placeholder='name'></input>
+        
+        <button onClick={()=>setColorlist([...colorlist,color])}>Add Color</button>
+           {colorlist.map((clr)=> <Colorbox color={clr} />)} 
+        
+        </div>
+            );
+        
+           
+        }
+        // export default Addcolor;
+        
+        function Colorbox({color}){
+            const styles={
+                backgroundColor:color,
+                height:"25px",
+                width:"250px"
+                
+            };
+            return(
+                <div style={styles}>
+        
+                </div>
+            );
+        }
